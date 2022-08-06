b643fe1fcd08cab0ac404380b9284d61dc0fb2de
GZRdev <gloriareilly@protonmail.com>
2022-08-06 11:23:05 -0700

    Added routing for Header/Footer/Home. Installed @mui/material @emotion/react @emotion/styled dependencies.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f44336",
+    },
+    secondary: {
+      main: "#ffef62",
+    },
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        <Footer/>
+      </ThemeProvider>
+    </>
+
   );
 }
 
